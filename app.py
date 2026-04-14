e9a902dfc9a359934251a317dff9bc9083d1d41f
sirranjeet-dev <sir.ranjeet@gmail.com>
2026-04-14 17:27:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 # Initialize FastAPI app
 app = FastAPI(title="MNIST CNN API")
-app.add_middleware(corsmiddleware,allow_origins=["*"],
+app.add_middleware(CORSMiddleware,allow_origins=["*"],
                    allow_redentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"],
